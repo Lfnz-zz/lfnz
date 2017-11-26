f76834feb26acec772c2c857d7b71b6c8e422b33
Luis Funes <lfnz999@gmail.com>
2017-11-26 01:25:19 -0600

    Contacto
    Modificacion del area de contacto

--- js/CrJs.js
+++ js/CrJs.js
@@ -2,4 +2,10 @@
     // the "href" attribute of the modal trigger must specify the modal ID that wants to be triggered
     $('#Venatna1').modal();
   });
-        
+
+ $(document).ready(function() {
+    $('select').material_select();
+  });
+
+  $('select').material_select('destroy');
+            
