3d5f5d966652eb1779c84037d407f21cb56e2644
Luis Funes <lfnz999@gmail.com>
2017-12-02 10:32:47 -0600

    actualizacion 10:23
    actualiza

--- pages/cv.php
+++ pages/cv.php
@@ -1,14 +1,17 @@
 <!-- Proyectos -->
 <section id="Cv">
-    <div class="container">
-        <div><h2>Servicios</h2></div> 
-        <div class="row">
-            <div class="col l4 CvAside">
-                <h4>Proyecto 1</h4>
-            </div>
-            <div class="col l8 CvBside">
-                <h4>Proyecto 2</h4>
-            </div>
-        </div>
+    <div class="row">
+        <div class="col l1">1</div>
+        <div class="col l1">2</div>
+        <div class="col l1">3</div>
+        <div class="col l1">4</div>
+        <div class="col l1">5</div>
+        <div class="col l1">6</div>
+        <div class="col l1">7</div>
+        <div class="col l1">8</div>
+        <div class="col l1">9</div>
+        <div class="col l1">10</div>
+        <div class="col l1">11</div>
+        <div class="col l1">12</div>
     </div>
 </section>
