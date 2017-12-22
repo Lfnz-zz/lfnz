ae57be3478bf2886f74f04f991d26c0f2fcfec29
Luis Funes <lfnz999@gmail.com>
2017-12-21 20:00:46 -0600

    xxx
    sjds

--- js/CrJs.js
+++ js/CrJs.js
@@ -8,4 +8,12 @@
   });
 
   $('select').material_select('destroy');
+
+var options = [
+    {selector: '.class', offset: 200, callback: customCallbackFunc },
+    {selector: '.other-class', offset: 200, callback: function() {
+      customCallbackFunc();
+    } },
+  ];
+  Materialize.scrollFire(options);
             
