d4c1e039ea98c0f020ebf60558d1e44ff657d1ce
Luis Funes <lfnz999@gmail.com>
2018-05-15 22:20:35 -0500

    actual
    actulizacion

--- includes/nav.php
+++ includes/nav.php
@@ -1,7 +1,7 @@
 <header>
     <nav lass="navbar fixed-top">
         <ul>
-            <li><a href="index.php?page=home">Home</a></li>
+            <li><a href="index.php?page=home">Staff</a></li>
             <li><a href="index.php?page=proyectos">Proyectos</a></li>
             <li><a href="index.php?page=bio">Acerca de</a></li>
             <li><a href="index.php?page=contact">Contacto</a></li>
